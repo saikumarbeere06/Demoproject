e92f5a2b41da3b7a994437afd64c131ed8f5c6ee
saikumarbeere06 <109521611+saikumarbeere06@users.noreply.github.com>
2022-07-28 11:58:45 +0530

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFirstProject.Models
+{
+    public class Employee
+    {
+        public int EmployeeId{ get;set; }
+        public string EmployeeNumber{ get;set; }
+        public string EmployeeLocation{ get;set; }
+        public string EmployeeName{ get;set; }
+    }
+}
